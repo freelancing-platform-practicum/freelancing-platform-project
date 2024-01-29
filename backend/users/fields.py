1377c529c70c7b5ea953d85a9dbbca3609604dee
Maxim Paramonov <maksimka@inbox.ru>
2024-01-29 11:10:49 +0300

    добавил валидацию расширения и размера графических файлов и pdf

--- backend/users/fields.py
+++ backend/users/fields.py
@@ -6,6 +6,8 @@ from django.core.exceptions import ValidationError
 from django.core.files.base import ContentFile
 from drf_extra_fields.fields import Base64FileField, Base64ImageField
 
+from taski.settings import ALLOWED_FILE_EXT, FILE_EXT_ERR
+
 # from rest_framework import serializers
 
 
@@ -42,7 +44,8 @@ class CustomizedBase64FileField(Base64FileField):
         ):
             format, content = data.split(';base64,')
             ext = format.split('/')[-1]
-            # добавить проверку расширения на допустимость типа
+            if ext not in [ALLOWED_FILE_EXT, 'pdf']:
+                raise ValidationError(FILE_EXT_ERR + ', .pdf')
             decoded_file = base64.b64decode(content)
             return ContentFile(
                 decoded_file,
@@ -58,24 +61,3 @@ class CustomizedBase64FileField(Base64FileField):
                     'data:image/' + extension + ';base64,', 'utf-8'
                 ) + base64.b64encode(file.read())
         return None
-
-
-'''
-class Base64ImageField(serializers.ImageField):
-    def to_internal_value(self, data):
-        if isinstance(data, str) and data.startswith('data:image'):
-            format, imgstr = data.split(';base64,')
-            ext = format.split('/')[-1]
-            data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
-        return super().to_internal_value(data)
-
-    def to_representation(self, value):
-        if value:
-            print(value.path)
-            print(value.extentions)
-            with open(value.path, 'rb') as image_file:
-                encoded_string = base64.b64encode(image_file.read())
-            print(encoded_string)
-            return encoded_string # super().to_representation(encoded_string)
-        return None
-'''
